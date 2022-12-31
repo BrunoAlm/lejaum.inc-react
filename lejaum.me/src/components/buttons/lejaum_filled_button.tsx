88c6a24c41ac5bd72dfff6a3dccaeceae78186a3
Bruno Almeida de Matos <65208006+BrunoAlm@users.noreply.github.com>
2022-12-31 00:31:56 -0300

    font georama filled button

--- lejaum.me/src/components/buttons/lejaum_filled_button.tsx
+++ lejaum.me/src/components/buttons/lejaum_filled_button.tsx
@@ -2,7 +2,7 @@ export default function FilledButton({ text = '', href = '', target = '_self'}) 
     return (
         <>
             <a href={href} target={target} className="bg-laranjaum w-44 h-fit py-2 rounded-3xl cursor-pointer" >
-                <p className="text-center text-branquin text-BUTTON uppercase">{text}</p>
+                <p className="text-center text-branquin text-BUTTON font-georama uppercase">{text}</p>
             </a>
         </>
     )
